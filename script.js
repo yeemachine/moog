63eed3c05d17e1c16056c6a55db5256fcb0f5360
Glitch (moogdoodle) <none>
2019-11-22 03:21:31 +0000

    🎉🍥 Updated with Glitch

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
             var lightR = new PIXI.lights.PointLight(0xffbf00, 3);
             var lightL = new PIXI.lights.PointLight(0xffbf00, 3);
             let displayText
+            let gameActive = false
 
             // put all layers for deferred rendering of normals
             stage.addChild(new PIXI.display.Layer(PIXI.lights.diffuseGroup));
@@ -16,6 +17,8 @@
                 .add('bg_normal', 'https://cdn.glitch.com/e352d3ca-2e03-47f1-acfd-675dff041f5f%2FNormalMap.png?v=1573974228592')
                 .add('block_diffuse', 'https://cdn.glitch.com/e352d3ca-2e03-47f1-acfd-675dff041f5f%2FTheremin.png?v=1573977008542')
                 .add('block_normal', 'https://cdn.glitch.com/e352d3ca-2e03-47f1-acfd-675dff041f5f%2FNormalMap.png?v=1573974228592')
+                .add('play_diffuse', 'https://cdn.glitch.com/e352d3ca-2e03-47f1-acfd-675dff041f5f%2Fplay.png?v=1574390546582')
+                .add('play_normal', 'https://cdn.glitch.com/e352d3ca-2e03-47f1-acfd-675dff041f5f%2FNormalMap.png?v=1573974228592')
                 .load(onAssetsLoaded);
 
             function createPair(diffuseTex, normalTex) {
@@ -32,37 +35,60 @@
             function onAssetsLoaded(loader, res) {
                 var bg = createPair(res.bg_diffuse.texture, res.bg_normal.texture);
                 var block = createPair(res.block_diffuse.texture, res.block_normal.texture);
+                var play = createPair(res.play_diffuse.texture, res.play_normal.texture);
+                var playLight = new PIXI.lights.PointLight(0xff7f00, 3);
                 let scale = .252
                 bg.scale = new PIXI.Point(scale,scale)
                 block.scale = new PIXI.Point(scale,scale)
+                play.scale = new PIXI.Point(scale,scale)
                 bg.position.set(0,0)
                 block.position.set(0,-6);
-                lightR.position.set(0, 0);
-                stage.addChild(bg);
-                stage.addChild(block);
+                play.position.set(270,140);
+                playLight.position.set(280,140);
+                lightR.visible = false
+                lightL.visible = false
+                stage.addChild(bg,block,play,playLight);
 
                 stage.addChild(new PIXI.lights.AmbientLight(null, 1));
                 stage.addChild(new PIXI.lights.DirectionalLight(null, 1, block));
                 stage.addChild(lightR,lightL);
-
-                bg.interactive = true;
-                bg.on('mouseover',()=>{
+              
+                stage.interactive = true
+              
+              
+                stage.on('pointerdown',(event)=>{
+                  play.visible = !play.visible
+                  playLight.visible = !playLight.visible
+                  lightR.visible = !lightR.visible
+                  lightL.visible = !lightL.visible
+                  lightR.position.copy(event.data.global);
+                  lightL.position.copy(event.data.global);
+                  gameActive = !gameActive
+                  getVideo()
+                  if(!gameActive){
+                    gainNode.gain.setTargetAtTime(calculateGain(0), context.currentTime, 0.001);
+                  }
+                  if(oscillator === null){
+                     createOscillator()
+                  }
+                })
+                              
+                stage.on('mouseover',()=>{
                   mouseOver = true
                 })
-                bg.on('mouseout',()=>{
+                stage.on('mouseout',()=>{
                   mouseOver = false
-                  gainNode.gain.setTargetAtTime(calculateGain(0), context.currentTime, 0.001);
+                  
                 })
-                bg.on('pointermove', function (event) {
+                stage.on('pointermove', function (event) {
+                  if(gameActive){
                     lightR.position.copy(event.data.global);
                     lightL.position.copy(event.data.global);
                     let formatedNote = generateNote(event.data.global.x,event.data.global.y)
                     displayText.text = formatedNote.scale + '|' + formatedNote.level
                     displayText.position.set(308-displayText.width/2,249)
                     changeFrequency(formatedNote.scaleNum,formatedNote.level)
-                    if(oscillator === null){
-                       createOscillator()
-                    }
+                  }
                 });
 
                 let warmLights= [{"x":138.23565673828125,"y":163.50717163085938}, {"x":240.69415283203125,"y":215.44491577148438}, {"x":565.5314331054688,"y":201.13427734375}, {"x":491.641357421875,"y":258.89849853515625}, {"x":495.38470458984375,"y":16.657516479492188}, {"x":71.86392211914062,"y":289.2891845703125}, {"x":199.52816772460938,"y":314.953125}, {"x":444.4444580078125,"y":317.1068115234375}, {"x":395.50982666015625,"y":263.0013427734375}, {"x":234.79013061523438,"y":268.43304443359375}]
